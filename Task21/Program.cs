d223ebdf8089abddc25f0fe69a246f01ff2c8638
Pavel <sivec_p@mail.ru>
2023-01-21 21:49:31 +0300

    Add Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,8 @@
+﻿int ax = Convert.ToInt32(Console.ReadLine());
+int ay = Convert.ToInt32(Console.ReadLine());
+int az = Convert.ToInt32(Console.ReadLine());
+int bx = Convert.ToInt32(Console.ReadLine());
+int by = Convert.ToInt32(Console.ReadLine());
+int bz = Convert.ToInt32(Console.ReadLine());
+double res = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+Console.WriteLine(res);
